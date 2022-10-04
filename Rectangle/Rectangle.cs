b6bf3771d2d25956a0a13b6662f3f1da36a31b14
Laura Lopez <yessica.lp@gmail.com>
2022-10-04 01:01:08 -0400

    The commit as containing the completed Rectangle class.

--- Rectangle/Rectangle.cs
+++ Rectangle/Rectangle.cs
@@ -8,5 +8,54 @@ namespace Rectangle
 {
     internal class Rectangle
     {
+        private int length =1;
+        private int width =1;
+
+        public Rectangle(int length, int width)
+        {
+            Length = length;
+            Width = width;
+        }
+
+        public Rectangle() { }
+
+        public int Length {
+            get { return this.length; }
+            set { length = value; }
+        
+        }
+        public int Width {
+            get { return this.width; } 
+            set { width = value; }
+        
+        }
+
+        public int GetCurrentLength()
+        {
+            return this.Length;
+        }
+
+        public int SetNewLength(int length)
+        {
+            this.Length = length;
+        }
+
+        public int GetCurrentWidth()
+        {
+            this.Width = this.width;
+        }
+
+        public int SetNewWidth(int width)
+        {
+            return this.Width = width;
+        }
+        public int GetPerimeter()
+        {
+            return (Width + Length)*2;
+        }
+        public int GetArea()
+        {
+            return (Length * Width);
+        }
     }
 }
